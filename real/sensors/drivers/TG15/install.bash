3f5ee2dfa6d4d7a64dd0f0d35f2dbbe2ada629e8
Takashi Mori <kanetugu2015@gmail.com>
2024-05-17 06:10:53 +0900

    bug fixed

--- real/sensors/drivers/TG15/install.bash
+++ real/sensors/drivers/TG15/install.bash
@@ -3,5 +3,5 @@
 THIRD_PARTY_PATH=../../../../third-party
 
 cp ydlidar_ros_driver/launch/* ${THIRD_PARTY_PATH}/ydlidar_ros2_driver/launch/
-cp ydlidar_ros_driver/param/* ${THIRD_PARTY_PATH}/ydlidar_ros2_driver/param/
+cp ydlidar_ros_driver/params/* ${THIRD_PARTY_PATH}/ydlidar_ros2_driver/params/
 cp -rp ${THIRD_PARTY_PATH}/ydlidar_ros2_driver ../../workspace/src/
