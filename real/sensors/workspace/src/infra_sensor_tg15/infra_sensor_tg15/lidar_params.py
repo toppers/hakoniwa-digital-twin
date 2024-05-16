f75746df4fc7ed5d0b58f31b009ac9afeb656b77
Takashi Mori <kanetugu2015@gmail.com>
2024-05-17 08:49:01 +0900

    parameter tuned

--- real/sensors/workspace/src/infra_sensor_tg15/infra_sensor_tg15/lidar_params.py
+++ real/sensors/workspace/src/infra_sensor_tg15/infra_sensor_tg15/lidar_params.py
@@ -1,7 +1,7 @@
 lidar_param_range_threshold = 0.0
 lidar_param_intensities_threshold = 1000.0
-lidar_param_range_average_num = 1
-lidar_param_scan_count_max = 10
+lidar_param_range_average_num = 10
+lidar_param_scan_count_max = 30
 lidar_param_segment_threshold = 0.1
 lidar_param_range_value_constant = 4.0
 lidar_param_sensor_b_degree = 0.0
@@ -9,6 +9,6 @@ lidar_param_sensor_mean_max = 10
 lidar_param_sensor_th_variance = 1.0
 lidar_param_sensor_t_radius = 0.1
 lidar_param_sensor_t_cv = 0.1
-lidar_param_sensor_t_radius_ok_min = 0.1
-lidar_param_sensor_t_radius_ok_max = 0.5
+lidar_param_sensor_t_radius_ok_min = 0.05
+lidar_param_sensor_t_radius_ok_max = 0.06
 
