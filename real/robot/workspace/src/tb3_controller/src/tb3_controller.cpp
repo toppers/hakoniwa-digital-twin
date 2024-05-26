917d0de994900ece300ef0a558552e1a0da90942
Takashi Mori <kanetugu2015@gmail.com>
2024-05-27 08:42:31 +0900

    add real robot controller

---
+++ real/robot/workspace/src/tb3_controller/src/tb3_controller.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main(int argc, char *argv[]) 
+{
+    const char *node_name = "tb3_controller_node";
+    std::cout << "START:" << node_name << std::endl;
+
+
+    return 0;
+}
+
