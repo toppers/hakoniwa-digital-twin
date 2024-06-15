15f299364eb51437f57a98b67582905c3e0f466e
Takashi Mori <kanetugu2015@gmail.com>
2024-06-15 14:53:46 +0900

    bug fixed

--- real/sensors/workspace/src/sensor_checker/sensor_checker/sensor_checker.py
+++ real/sensors/workspace/src/sensor_checker/sensor_checker/sensor_checker.py
@@ -8,13 +8,15 @@ import sys
 from .real_time_plot import RealTimePlot
 
 global plotter
+plotter = None
 
 class SensorChecker(Node):
     def __init__(self):
         super().__init__('sensor_checker')
-        qos_profile = QoSProfile(depth=10,
-                                 reliability=ReliabilityPolicy.BEST_EFFORT,
-                                 durability=DurabilityPolicy.VOLATILE)
+        qos_profile = QoSProfile(
+            depth=10,
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.VOLATILE)
         self.subscription = self.create_subscription(
             LaserScan,
             '/scan',
@@ -29,17 +31,15 @@ class SensorChecker(Node):
     def listener_callback(self, msg):
         global plotter
         print(f"len: {len(msg.ranges)}")
-        #if plotter is not None:
-        #    plotter.add_data(msg.ranges[400])
-        
-
+        if plotter is not None:
+            plotter.add_data(msg.ranges[400])
 
 def spin_node(node):
     rclpy.spin(node)
 
 def main(args=None):
     global plotter
-    #app = QApplication(sys.argv)
+    plotter = RealTimePlot()
     rclpy.init(args=args)
     lidar_subscriber = SensorChecker()
     lidar_subscriber.get_logger().info("SensorChecker UP")
@@ -47,9 +47,7 @@ def main(args=None):
     spin_thread = threading.Thread(target=spin_node, args=(lidar_subscriber,))
     spin_thread.start()
 
-    plotter = RealTimePlot()
     plotter.start()
-
 
 if __name__ == '__main__':
     main()
