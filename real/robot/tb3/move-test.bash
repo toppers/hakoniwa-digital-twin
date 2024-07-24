5dd69e754a541fd8d3372964b1c84c73969c6122
Takashi Mori <kanetugu2015@gmail.com>
2024-07-24 09:35:04 +0900

    add move test

---
+++ real/robot/tb3/move-test.bash
@@ -0,0 +1,21 @@
+!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 {f|s|b}"
+    exit 1
+fi
+
+OP=${1}
+
+if [ ${OP} = "f" ]
+then
+    ros2 topic pub /tb3_cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.05, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
+elif [ ${OP} = "s" ]
+then
+    ros2 topic pub /tb3_cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
+elif [ ${OP} = "b" ]
+then
+    ros2 topic pub /tb3_cmd_vel geometry_msgs/msg/Twist "{linear: {x: -0.05, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
+fi
+
