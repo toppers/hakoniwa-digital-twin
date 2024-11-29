c6ce4e781c117b38dc7c995b388f9f23d76d86ca
Takashi Mori <kanetugu2015@gmail.com>
2024-11-29 15:24:03 +0900

    add ar config and run

--- run.bash
+++ run.bash
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+CUSTOM_JSON=`pwd`/digital/config/ar/custom.json
+
+HAKONIWA_WEB_SERVER_PATH=`pwd`/../hakoniwa-webserver
+HAKONIWA_AR_BRIDGE_PATH=`pwd`/../si2024/hakoniwa-ar-bridge
+HAKONIWA_PX4SIM_PATH=`pwd`/../hakoniwa-px4sim/hakoniwa
+HAKONIWA_SHMPROXY_PATH=bridge/virtual
+HAKONIWA_UNITY_APL_PATH=`pwd`/../si2024/hakoniwa-unity-drone-model/DTwinAppleSilicon
+export PYTHONPATH="/usr/local/lib/hakoniwa:$PYTHONPATH"
+export PYTHONPATH="/usr/local/lib/hakoniwa/py:$PYTHONPATH"
+export PATH="/usr/local/bin/hakoniwa:${PATH}"
+export LD_LIBRARY_PATH="/usr/local/lib/hakoniwa:${LD_LIBRARY_PATH}"
+export DYLD_LIBRARY_PATH="/usr/local/lib/hakoniwa:${DYLD_LIBRARY_PATH}"
+
+if [ -f ~/myenv/bin/activate  ]
 then
-    echo "Usage: $0 {real|sim|ar}"
-    exit 1
+    source ~/myenv/bin/activate
+    PYTHON_BIN=python3.12
+else
+    PYTHON_BIN=python
 fi
 
-ACT_MODE=$1
-CUSTOM_JSON=`pwd`/digital/config/${ACT_MODE}/custom.json
-
-HAKONIWA_RADIO_CTRL_PATH=../hakoniwa-px4sim/drone_api/sample
-HAKONIWA_PX4SIM_PATH=../hakoniwa-px4sim/hakoniwa
-HAKONIWA_SHMPROXY_PATH=bridge/virtual
-HAKONIWA_VREAL_PATH=../hakoniwa-unity-drone-model
 
 HAKO_PX4SIM_PID=
 HAKO_SHMPROXY_PID=
-HAKO_VREAL_PID=
+HAKO_UNITY_PID=
+HAKO_WEBSRV_PID=
 function kill_process()
 {
     echo "trapped"
@@ -26,10 +35,15 @@ function kill_process()
         kill -s TERM "$HAKO_SHMPROXY_PID" || echo "Failed to kill shmproxy"
     fi
 
-    if [ ! -z "$HAKO_VREAL_PID" ]
+    if [ ! -z "$HAKO_UNITY_PID" ]
     then
-        echo "KILLING: vreal $HAKO_VREAL_PID"
-        kill -s TERM "$HAKO_VREAL_PID" || echo "Failed to kill vreal"
+        echo "KILLING: unity $HAKO_UNITY_PID"
+        kill -s TERM "$HAKO_UNITY_PID" || echo "Failed to kill unity"
+    fi
+    if [ ! -z "$HAKO_WEBSRV_PID" ]
+    then
+        echo "KILLING: web server $HAKO_WEBSRV_PID"
+        kill -s TERM "$HAKO_WEBSRV_PID" || echo "Failed to kill web server"
     fi
     if [ ! -z "$HAKO_PX4SIM_PID" ]
     then
@@ -44,18 +58,11 @@ function signal_handler() {
 }
 trap signal_handler SIGINT
 
-export PYTHONPATH="/usr/local/lib/hakoniwa:$PYTHONPATH"
-export PYTHONPATH="/usr/local/lib/hakoniwa/py:$PYTHONPATH"
-export PATH="/usr/local/bin/hakoniwa:${PATH}"
-export LD_LIBRARY_PATH="/usr/local/lib/hakoniwa:${LD_LIBRARY_PATH}"
-export DYLD_LIBRARY_PATH="/usr/local/lib/hakoniwa:${DYLD_LIBRARY_PATH}"
-
-
 function activate_px4sim()
 {
     CURR_DIR=`pwd`
     cd $HAKONIWA_PX4SIM_PATH
-    bash drone-app.bash ../../hakoniwa-unity-drone-model/ar-demo-ros/ config/rc &
+    bash drone-app.bash ${HAKONIWA_UNITY_APL_PATH} config/rc &
     HAKO_PX4SIM_PID=$!
     cd $CURR_DIR
 }
@@ -67,71 +74,44 @@ function activate_shmproxy()
     HAKO_SHMPROXY_PID=$!
     cd $CURR_DIR
 }
-function activate_vreal()
+function activate_unity()
 {
     CURR_DIR=`pwd`
-    cd $HAKONIWA_VREAL_PATH
-    bash plugin/activate_app.bash TwinReal &
-    HAKO_VREAL_PID=$!
-    cd $CURR_DIR
-}
-function activate_ardemo()
-{
-    CURR_DIR=`pwd`
-    CONFIG_PATH=`pwd`/digital/config/ar/xr_config.json 
-    cp $CONFIG_PATH $HAKONIWA_VREAL_PATH/ar-demo-ros/
-    cd $HAKONIWA_VREAL_PATH
-    bash plugin/activate_app.bash ar-demo-ros &
-    HAKO_VREAL_PID=$!
-    cd $CURR_DIR
-}
-function adjust_initial_pos()
-{
-    CONFIG_PATH=`pwd`/digital/config/ar/xr_config.json
-    AR_DEVICE_IPADDR=`jq -r '.server_url' digital/config/ar/xr_config.json | awk -F: '{print $1}'`
-    CURR_DIR=`pwd`
-    cd $HAKONIWA_VREAL_PATH
-    echo "access: ${AR_DEVICE_IPADDR}:38528"
-    python ${HAKONIWA_VREAL_PATH}/utils/xr_origin_tuning.py --input joystick ${CONFIG_PATH} ${AR_DEVICE_IPADDR}:38528
+    cd $HAKONIWA_UNITY_APL_PATH
+    cd ..
+    bash plugin/activate_app.bash DTwinAppleSilicon &
+    HAKO_UNITY_PID=$!
     cd $CURR_DIR
 }
 
-function radio_control()
+function activate_web_server()
 {
     CURR_DIR=`pwd`
-    cd $HAKONIWA_RADIO_CTRL_PATH
-    if [ -f ~/myenv/bin/activate  ]
-    then
-        source ~/myenv/bin/activate
-        PYTHON_BIN=python3.12
-    else
-        PYTHON_BIN=python
-    fi
-    ${PYTHON_BIN} rc.py ../../../hakoniwa-unity-drone-model/ar-demo-ros/custom.json
+    cd $HAKONIWA_WEB_SERVER_PATH
+    ${PYTHON_BIN} server/main.py --asset_name WebServer --config_path config/twin-custom.json --delta_time_usec 20000 &
+    HAKO_WEBSRV_PID=$!
+    cd $CURR_DIR
+}
+
+function activate_ar_bridge()
+{
+    CURR_DIR=`pwd`
+    CONFIG_PATH=`pwd`/digital/config/ar/ar_bridge_config.json 
+    cd $HAKONIWA_AR_BRIDGE_PATH
+    ${PYTHON_BIN} asset_lib/main.py --config ${CONFIG_PATH}
     cd $CURR_DIR
 }
 
 activate_px4sim
 
-if [ "${ACT_MODE}" = "sim" ]
-then
-    sleep 1
-    activate_vreal
-fi
+sleep 1
+
+activate_unity
 
 sleep 1
-if [ "${ACT_MODE}" == "ar" ]
-then
-    activate_shmproxy
-    activate_ardemo
-    #echo "Start Unity"
-    #read
-else
-    activate_shmproxy
-    sleep 1
-    echo "Start Unity"
-    read
-fi
+
+activate_shmproxy
+
 
 sleep 5
 
@@ -139,10 +119,8 @@ hako-cmd start
 
 sleep 1
 
-echo "START ADJUST INITIAL POSITION"
-adjust_initial_pos
+activate_web_server
 
-echo "START RADIO CONTROL"
-radio_control
+activate_ar_bridge
 
 kill_process
